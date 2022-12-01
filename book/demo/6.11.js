46a745d6081ace024a0d6d602a339318e9f7b660
kingdoublecool <30cool@sina.com>
2022-12-01 14:58:00 +0800

    Demo, 0611: refactoring

--- book/demo/6.11.js
+++ book/demo/6.11.js
@@ -1,10 +1,16 @@
 function priceOrder(product, quantity, shippingMethod) {
+ const priceData = calculatePricingData(product, quantity);
+ return applyShipping(priceData, shippingMethod);
+}
+function calculatePricingData(product, quantity) {
  const basePrice = product.basePrice * quantity;
  const discount = Math.max(quantity - product.discountThreshold, 0)
      * product.basePrice * product.discountRate;
- const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+ return {basePrice: basePrice, quantity: quantity, discount:discount};
+}
+function applyShipping(priceData, shippingMethod) {
+ const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
      ? shippingMethod.discountedFee : shippingMethod.feePerCase;
- const shippingCost = quantity * shippingPerCase;
- const price = basePrice - discount + shippingCost;
- return price;
+ const shippingCost = priceData.quantity * shippingPerCase;
+ return priceData.basePrice - priceData.discount + shippingCost;
 }
