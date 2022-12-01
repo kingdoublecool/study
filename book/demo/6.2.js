1f6d7484b64fe3b14d8e1b1b4e2ea8cb4b8e7e8e
kingdoublecool <30cool@sina.com>
2022-12-01 13:10:14 +0800

    Demo, 0602: demo

---
+++ book/demo/6.2.js
@@ -0,0 +1,11 @@
+function reportLines(aCustomer) {
+  const lines = [];
+  lines.push(["name", aCustomer.name]);
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
+}
