a56471f879c44f7b1884d3ab88c2c672fd91557c
kingdoublecool <30cool@sina.com>
2022-12-01 14:47:13 +0800

    Demo, 0610: refactoring

--- book/demo/6.10.js
+++ book/demo/6.10.js
@@ -1,18 +1,28 @@
 reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 # File 1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 # File 2
-const aReading = acquireReading();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const base = aReading.baseCharge;
+const taxableCharge = aReading.taxableCharge;
 
 # File 3
-const aReading = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading);
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const basicChargeAmount = aReading.baseCharge;
 
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, result.baseCharge - taxThreshold(result.year));
+  return result;
 }
