3879a341d4e7491b06714bb1103a2c4dc656f422
kingdoublecool <30cool@sina.com>
2022-12-01 12:42:35 +0800

    Demo, 0601: refactoring.1

--- book/demo/6.1.1.js
+++ book/demo/6.1.1.js
@@ -1,25 +1,36 @@
 function printOwing(invoice) {
   let outstanding = 0;
+  printBanner();
+  const outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
 
-  console.log("***********************");
-  console.log("**** Customer Owes ****");
-  console.log("***********************");
-
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
+  function printBanner() {
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
   }
 
-  // record due date
-  const today = Clock.today;
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+  function printDetails(invoice, outstanding) {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  }
 
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 30
+    );
+  }
+
+  function calculateOutstanding(invoice) {
+    let result = 0;
+    for (const o of invoice.orders) {
+      result += o.amount;
+    }
+    return result;
+  }
 }
