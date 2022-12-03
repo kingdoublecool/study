d2ba6dbfc6c8219a51ca3fe60ca83dc7742f4576
kingdoublecool <30cool@sina.com>
2022-12-03 19:49:10 +0800

    Demo, 0703: refactoring

--- book/demo/7.3.js
+++ book/demo/7.3.js
@@ -3,11 +3,30 @@ class Order() {
     this.priority = data.priority;
     // more initialization
   }
+  get priority() {return this._priority;}
+  get priorityString() {return this._priority.toString();}
+  set priority(aString) {this._priority = new Priority(aString);}
+}
+
+class Prioity {
+  constructor(value) {
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value))
+      this._value = value;
+    else
+      throw new Error(`<${value}> is invalid for Priority`);
+}
+  toString() {return this._value;}
+  get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+  static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+  
+  equals(other) {return this._index === other._index;}
+  higherThan(other) {return this._index > other._index;}
+  lowerThan(other) {return this._index < other._index;}
 }
 
 highPriorityCount = orders
-  .filter(o => "high" === o.priority ||
-                "rush" === o.priority)
+  .filter(o => o.priority.higherThan(new Priority("normal")))
   .length
 ;
 
