0c15f59b309454134abd8246f4a73c986ef6aad9
kingdoublecool <30cool@sina.com>
2022-12-04 20:08:00 +0800

    Demo, 0709: refactoring

--- book/demo/7.9.js
+++ book/demo/7.9.js
@@ -1,15 +1,5 @@
 function foundPerson(people) {
-  for(let i = 0; i < people.length; i++) { 
-    if (people[i] === "Don") {
-      return "Don";
-    }
-    if (people[i] === "John") { 
-      return "John";
-    }
-    if (people[i] === "Kent") { 
-      return "Kent";
-    }
-  }
-  return "";
+  const candidates = ["Don", "John", "Kent"];
+  return people.find(p => candidates.includes(p)) || '';
 }
 
