901011983fd656638331208ada1a8e2daf56bf32
kingdoublecool <30cool@sina.com>
2022-12-04 17:22:05 +0800

    Demo, 0706: refactoring

--- book/demo/7.6.js
+++ book/demo/7.6.js
@@ -1,19 +1,10 @@
-class TrackingInformation {
+class Shipment {
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
   get shippingCompany()    {return this._shippingCompany;} 
   set shippingCompany(arg) {this._shippingCompany = arg;} 
   get trackingNumber()    {return this._trackingNumber;} 
   set trackingNumber(arg) {this._trackingNumber = arg;} 
-  get display()            {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
 }
-class Shipment {
-  get trackingInfo() {
-    return this._trackingInformation.display;
-  }
-  get trackingInformation() {return this._trackingInformation;} 
-  set trackingInformation(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation;
-  }
-}
-aShipment.trackingInformation.shippingCompany = request.vendor;
+aShipment.shippingCompany = request.vendor;
