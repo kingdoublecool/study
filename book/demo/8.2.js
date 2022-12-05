4d5495befa6fb0da9d24b64025a81acc34d6883b
kingdoublecool <30cool@sina.com>
2022-12-05 13:46:29 +0800

    Demo, 0802: refactoring

--- book/demo/8.2.js
+++ book/demo/8.2.js
@@ -1,20 +1,24 @@
 class Customer {
   constructor(name, discountRate) {
     this._name = name;
-    this._discountRate = discountRate;
+    this._setDiscountRate(discountRate);
     this._contract = new CustomerContract(dateToday());
   }
-  get discountRate() {return this._discountRate;}
+  get discountRate() {return this._contract.discountRate;}
+  _setDiscountRate(aNumber) {this._contract.discountRate = aNumber;}
   becomePreferred() {
-    this._discountRate += 0.03;
+    this._setDiscountRate(this.discountRate + 0.03);
     // other nice things
   }
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this._discountRate));
+    return amount.subtract(amount.multiply(this.discountRate));
   }
 }
 class CustomerContract {
-  constructor(startDate) {
+  constructor(startDate, discountRate) {
     this._startDate = startDate;
+    this._discountRate = discountRate;
   }
+  get discountRate()  {return this._discountRate;}
+  set discountRate(arg) {this._discountRate = arg;}
 }
