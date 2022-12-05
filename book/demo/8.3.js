2745611d87c84946e21f5d8fd7755cdb4f0ca838
kingdoublecool <30cool@sina.com>
2022-12-05 14:25:07 +0800

    Demo, 0803: refactoring

--- book/demo/8.3.js
+++ book/demo/8.3.js
@@ -2,21 +2,20 @@ function renderPerson(outStream, person) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>title: ${person.photo.title}</p>`);
   result.push(emitPhotoData(person.photo));
   return result.join("\n");
 }
 function photoDiv(p) {
   return [
     "<div>",
-    `<p>title: ${p.title}</p>`,
-    emitPhotoData(p),
+    emitPhotoData(aPhoto),
     "</div>",
   ].join("\n");
 }
 function emitPhotoData(aPhoto) {
-  const result = [];
-  result.push(`<p>location: ${aPhoto.location}</p>`);
-  result.push(`<p>date: ${aPhoto.date.toDateString()}</p>`);
-  return result.join("\n");
+  return [
+    `<p>title: ${aPhoto.title}</p>`,
+    `<p>location: ${aPhoto.location}</p>`,
+    `<p>date: ${aPhoto.date.toDateString()}</p>`,
+  ].join("\n");
 }
