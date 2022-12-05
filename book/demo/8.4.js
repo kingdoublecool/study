2a09f313b08f4f6e6c6b50d15e45aadb8250cfb8
kingdoublecool <30cool@sina.com>
2022-12-05 14:37:48 +0800

    Demo, 0804: refactoring

--- book/demo/8.4.js
+++ book/demo/8.4.js
@@ -2,18 +2,19 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 function listRecentPhotos(outStream, photos) {
   photos
     .filter(p => p.date > recentDateCutoff())
     .forEach(p => {
      outStream.write("<div>\n");
-     emitPhotoData(outStream, p);
+     emitPhotoData(outStream, person.photo);
+     outStream.write(`<p>location: ${photo.location}</p>\n`);
      outStream.write("</div>\n");
   });
 }
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
