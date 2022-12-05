e022520cb0c31be1078294721278361188fb578e
kingdoublecool <30cool@sina.com>
2022-12-05 15:47:43 +0800

    Demo, 0807: refactoring

--- book/demo/8.7.js
+++ book/demo/8.7.js
@@ -1,7 +1,8 @@
-let youngest = people[0] ? people[0].age : Infinity;
-let totalSalary = 0;
-for (const p of people) {
-  if (p.age < youngest) youngest = p.age;
-  totalSalary += p.salary;
-}
 return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+function totalSalary() {
+  return people.reduce((total,p) => total + p.salary, 0);
+}
+function youngestAge() {
+  return Math.min(...people.map(p => p.age));
+}
+
