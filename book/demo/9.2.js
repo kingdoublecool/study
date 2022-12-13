9b7558948353de095d963487e08218b199d230e1
kingdoublecool <30cool@sina.com>
2022-12-13 13:38:20 +0800

    Demo, 0902: refactoring

--- book/demo/9.2.js
+++ book/demo/9.2.js
@@ -1,11 +1,11 @@
 class Organization {
  constructor(data) {
-  this._name = data.name;
+  this._title = data.title;
   this._country = data.country;
  }
- get name() {return this._name;}
- set name(aString) {this._name = aString;}
- get country() {return this._country;}
+ get title() {return this._title;}
+ set title(aString) {this._title = aString;}
+ get country()  {return this._country;}
  set country(aCountryCode) {this._country = aCountryCode;}
 }
-const organization = new Organization({name: "Acme Gooseberries", country: "GB"});
+const organization = new Organization({title: "Acme Gooseberries", country: "GB"});
