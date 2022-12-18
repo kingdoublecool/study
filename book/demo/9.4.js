337596cbae8baf1e0d32f4cf8cd1e342a8164b3a
kingdoublecool <30cool@sina.com>
2022-12-18 12:34:48 +0800

    Demo, 0904: refactoring

--- book/demo/9.4.js
+++ book/demo/9.4.js
@@ -6,17 +6,21 @@ class Person {
     return this._telephoneNumber.areaCode;
   }
   set officeAreaCode(arg) {
-    this._telephoneNumber.areaCode = arg;
+    this._telephoneNumber.areaCode = new TelephoneNumber(arg, this.officeNumber);
   }
   get officeNumber() {
     return this._telephoneNumber.number;
   }
   set officeNumber(arg) {
-    this._telephoneNumber.number = arg;
+    this._telephoneNumber.number = new TelephoneNumber(this.officeAreaCode, age);
   }
 }
 
 class TelephoneNumber {
+  constructor(areaCode, number) { 
+    this._areaCode = areaCode; 
+    this._number = number;
+  }
   get areaCode() {
     return this._areaCode;
   } 
@@ -29,4 +33,14 @@ class TelephoneNumber {
   set number(arg) {
     this._number = arg;
   }
+  equals(other) {
+　  if (!(other instanceof TelephoneNumber)) return false; 
+　  return this.areaCode === other.areaCode &&
+　　  this.number === other.number;
+  }
 }
+
+# Test
+it('telephone equals', function() {
+  assert(new TelephoneNumber("312", "555-0142").equals(new TelephoneNumber("312", "555-0142")));
+});
