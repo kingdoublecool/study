5d7cbc84ed0adddf78f94ad364d6348aff555df1
kingdoublecool <30cool@sina.com>
2022-12-18 15:38:46 +0800

    Demo, 1003: refactoring

--- book/demo/10.3.js
+++ book/demo/10.3.js
@@ -1,20 +1,15 @@
 function payAmount(employee) { 
   let result; 
   if(employee.isSeparated) {
-    result = {amount: 0, reasonCode:"SEP"};
+    return {amount: 0, reasonCode:"SEP"};
   }
-  else {
-    if (employee.isRetired) {
-      result = {amount: 0, reasonCode: "RET"};
-    }
-    else {
-      // logic to compute amount 
-      lorem.ipsum(dolor.sitAmet);1
-      consectetur(adipiscing).elit();
-      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua); 
-      ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-    }
+  if (employee.isRetired) {
+    return {amount: 0, reasonCode: "RET"};
   }
-  return result;
+  // logic to compute amount 
+  lorem.ipsum(dolor.sitAmet);1
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua); 
+  ut.enim.ad(minim.veniam);
+  return someFinalComputation();
 }
