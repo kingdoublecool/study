bf256c73e1c8598a1d01a65dbf62d01bff9cd5bc
kingdoublecool <30cool@sina.com>
2022-12-29 15:36:57 +0800

    Demo, 1101: demo

---
+++ book/demo/11.1.js
@@ -0,0 +1,14 @@
+function alertForMiscreant (people) { 
+  for (const p of people) {
+    if (p === "Don") { 
+      setOffAlarms(); 
+      return "Don";
+    }
+    if (p === "John") {
+      setOffAlarms();
+      return "John";
+    }
+  }
+  return "";
+}
+const found = alertForMiscreant(people);
