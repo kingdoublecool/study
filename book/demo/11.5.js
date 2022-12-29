d1b119c3990b4145fd5ba8d30c2010fa1e2dcd25
kingdoublecool <30cool@sina.com>
2022-12-29 17:06:13 +0800

    Demo, 1105: refactoring

--- book/demo/11.5.js
+++ book/demo/11.5.js
@@ -1,16 +1,16 @@
 class Order {
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice; 
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2; 
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+    return this.discountedPrice(basePrice);
   }
-
-  discountedPrice(basePrice, discountLevel) { 
-    switch (discountLevel) {
+  discountedPrice(basePrice) { 
+    switch (this.discountLevel) {
     case 1: return basePrice * 0.95;
     case 2: return basePrice * 0.9;
     }
+  }
+
+  get discountLevel() {
+    return (this.quantity > 100) ? 2 : 1;
   }
 }
