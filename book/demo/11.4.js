f911d865ff56c425522f143ce2b1c8595b21d157
kingdoublecool <30cool@sina.com>
2022-12-29 16:43:37 +0800

    Demo, 1104: refactoring

--- book/demo/11.4.js
+++ book/demo/11.4.js
@@ -1,12 +1,10 @@
 class HeatingPlan() {
-  withinRange(bottom, top) {
-    return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
+  withinRange(aNumberRange) {
+    return (aNumberRange.low >= this._temperatureRange.low) && (aNumberRange.high <= this._temperatureRange.high);
   }
 }
 
 aPlan = new HeatingPlan();
-const low = aRoom.daysTempRange.low; 
-const high = aRoom.daysTempRange.high; 
-if (!aPlan.withinRange(low, high))
+if (!aPlan.withinRange(aRoom.daysTempRange)) 
   alerts.push("room temperature went outside range");
 
