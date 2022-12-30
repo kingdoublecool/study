d8caac9a9cac6a4f7ec97f41a9b7c69ee6ede715
kingdoublecool <30cool@sina.com>
2022-12-30 09:21:05 +0800

    Demo, 1107: refactoring

--- book/demo/11.7.js
+++ book/demo/11.7.js
@@ -1,9 +1,10 @@
 class Person {
+  constructor(id) {
+    this._id = id
+  }
   get name() {return this._name;} 
   set name(arg) {this._name = arg;} 
   get id() {return this._id;}
-  set id(arg) {this._id = arg;}
 }
-const martin = new Person();
+const martin = new Person("1234");
 martin.name = "martin";
-martin.id = "1234";
