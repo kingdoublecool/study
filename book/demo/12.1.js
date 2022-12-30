f288e844d7200e1db8257280ffdc8efbc9bae780
kingdoublecool <30cool@sina.com>
2022-12-30 13:32:40 +0800

    Demo 1201: refactoring

--- book/demo/12.1.js
+++ book/demo/12.1.js
@@ -1,15 +1,13 @@
 class Party {
-
-}
-
-class Employee extends Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
 
+class Employee extends Party {
+
+}
+
 class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
+
 }
