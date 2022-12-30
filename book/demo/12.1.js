d91d246f162334ddd3460d083dac4beac03e2d31
kingdoublecool <30cool@sina.com>
2022-12-30 13:29:05 +0800

    Demo ,1201: demo

---
+++ book/demo/12.1.js
@@ -0,0 +1,15 @@
+class Party {
+
+}
+
+class Employee extends Party {
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  get totalAnnualCost() {
+    return this.monthlyCost * 12;
+  }
+}
