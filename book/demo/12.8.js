b7866997c1fa89902cec1f90298bbf286021772f
kingdoublecool <30cool@sina.com>
2022-12-30 18:17:35 +0800

    Demo, 1208: refactoring

--- book/demo/12.8.js
+++ book/demo/12.8.js
@@ -1,23 +1,27 @@
-class Employee {
-  constructor(name, id, monthlyCost) {
-    this._id = id;
-    this._name = name; 
-    this._monthlyCost = monthlyCost;
+class Party {
+  constructor(name) {
+    this._name = name;
   }
-  get monthlyCost() {return this._monthlyCost;} 
   get name() {return this._name;}
-  get id() {return this._id;}
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
-class Department {
+class Employee extends Party {
+  constructor(name, id, monthlyCost) {
+    super(name);
+    this._id = id;
+    this._monthlyCost = monthlyCost;
+  }
+  get monthlyCost() {return this._monthlyCost;} 
+  get id() {return this._id;}
+}
+class Department extends Party{
   constructor(name, staff) {
-    this._name = name;
+    super(name);
     this._staff = staff;
   }
   get staff() {return this._staff.slice();}
-  get name() {return this._name;}
   get totalMonthlyCost() {
     return this.staff
       .map(e => e.monthlyCost)
