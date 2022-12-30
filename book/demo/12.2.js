bf39989ae1cedf14556038cd37e3047ac47e1781
kingdoublecool <30cool@sina.com>
2022-12-30 13:39:14 +0800

    Demo, 1202: refactoring

--- book/demo/12.2.js
+++ book/demo/12.2.js
@@ -1,9 +1,7 @@
-class Employee {...}
-
-class Salesman extends Employee {
-  private String name;
+class Employee {
+  protected String name;
 }
 
-class Engineer extends Employee {
-  private String name;
-}
+class Salesman extends Employee {...}
+
+class Engineer extends Employee {...}
