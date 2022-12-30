dabc5ea6dc0afe4f956e5439e97f60d37e14708b
kingdoublecool <30cool@sina.com>
2022-12-30 15:57:50 +0800

    Demo, 1207: demo

---
+++ book/demo/12.7.js
@@ -0,0 +1,36 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {return this._name;}
+  get genderCode() {return "X";}
+}
+class Male extends Person {
+  get genderCode() {return "M";}
+}
+class Female extends Person {
+  get genderCode() {return "F";}
+}
+function createPerson(name) {
+  return new Person(name);
+}
+function createMale(name) {
+  return new Male(name);
+}
+function createFemale(name) {
+  return new Female(name);
+}
+function loadFromInput(data) {
+  const result = [];
+  data.forEach(aRecord => {
+    let p;
+    switch (aRecord.gender) {
+      case 'M': p = new Male(aRecord.name); break; 
+      case 'F': p = new Female(aRecord.name); break; 
+      default: p = new Person(aRecord.name);
+    }
+    result.push(p);
+  });
+  return result;
+}
+const numberOfMales = people.filter(p => p instanceof Male).length;
