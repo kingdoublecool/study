8a2b2beca12dec564b896ab19640b4829647a264
kingdoublecool <30cool@sina.com>
2022-12-30 15:49:45 +0800

    Demo, 1206: refactoring

--- book/demo/12.6.js
+++ book/demo/12.6.js
@@ -1,14 +1,29 @@
 class Employee {
-  constructor(name, type){ 
-    this.validateType(type); 
+  constructor(name){ 
     this._name = name; 
-    this._type = type;
   }
-  validateType(arg) {
-    if (!["engineer", "manager", "salesman"].includes(arg))
-      throw new Error(`Employee cannot be of type ${arg}`);
+  function createEmployee(name, type) {
+    switch (type) {
+      case "enginerr":
+        return new Employee(name);
+      case "manager": 
+        return new Manager(name);
+      case "salesman":
+        return new Salesman(name);
+      default: 
+        throw new Error(`Employee cannot be of type ${type}`);
+    }
   }
   toString() {
-    return `${this._name} (${this._type})`;
+    return `${this._name} (${this.type})`;
   }
+}
+class Engineer extends Employee {
+  get type() {return "engineer";}
+}
+class Salesman extends Employee {
+  get type() {return "salesman";}
+}
+class Manager extends Employee {
+  get type() {return "manager";}
 }
