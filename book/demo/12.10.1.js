411e310f1b334768a6eebf7fbc1fb7a1af9b78bf
kingdoublecool <30cool@sina.com>
2023-01-03 14:50:22 +0800

    Demo, 1210: refactoring

--- book/demo/12.10.1.js
+++ book/demo/12.10.1.js
@@ -1,47 +1,58 @@
 function createBird(data) {
-  switch (data.type) {
-  case 'EuropeanSwallow':
-    return new EuropeanSwallow(data);
-  case 'AfricanSwallow':
-    return new AfricanSwallow(data);
-  case 'NorweigianBlueParrot':
-    return new NorwegianBlueParrot(data);
-  default:
-    return new Bird(data);
- }
+  return new Bird(data);
 }
 class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
-   }
-  get name() {return this._name;}
-  get plumage() {
-    return this._plumage || "average";
+    this._speciesDelegate = this.selectSpeciesDelegate(data);
   }
-  get airSpeedVelocity() {return null;}
+  get name() {return this._name;}
+  get plumage() {return this._speciesDelegate.plumage;}
+  get airSpeedVelocity() {return this._speciesDelegate.airSpeedVelocity;}
+
+  selectSpeciesDelegate(data) {
+    switch(data.type) {
+    case 'EuropeanSwallow':
+      return new EuropeanSwallowDelegate(data, this);
+    case 'AfricanSwallow':
+      return new AfricanSwallowDelegate(data, this);
+    case 'NorweigianBlueParrot':
+      return new NorwegianBlueParrotDelegate(data, this);
+    default: return new SpeciesDelegate(data, this);
+    }
+  }
 }
-class EuropeanSwallow extends Bird {
+class SpeciesDelegate {
+ constructor(data, bird) {
+  this._bird = bird;
+ }
+ get plumage() {
+  return this._bird._plumage || "average";
+ }
+ get airSpeedVelocity() {return null;}
+}
+class EuropeanSwallowDelegate extends SpeciesDelegate{
   get airSpeedVelocity() {return 35;}
 }
-class AfricanSwallow extends Bird {
-  constructor(data) {
-    super (data);
+class AfricanSwallowDelegate extends SpeciesDelegate{
+  constructor(data, bird) {
+    super(data, bird);
     this._numberOfCoconuts = data.numberOfCoconuts;
   }
   get airSpeedVelocity() {
     return 40 - 2 * this._numberOfCoconuts;
   }
 }
-class NorwegianBlueParrot extends Bird {
-  constructor(data) {
-    super (data);
+class NorwegianBlueParrotDelegate extends SpeciesDelegate{
+  constructor(data, bird) {
+    super(data, bird);
     this._voltage = data.voltage;
     this._isNailed = data.isNailed;
   }
   get plumage() {
     if (this._voltage > 100) return "scorched";
-    else return this._plumage || "beautiful";
+    else return this._bird._plumage || "beautiful";
   }
   get airSpeedVelocity() {
     return (this._isNailed) ? 0 : 10 + this._voltage / 10;
--- book/demo/12.10.js
+++ book/demo/12.10.js
@@ -4,28 +4,48 @@ class Booking {
     this._date = date;
   }
   get hasTalkback() {
-    return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+    return (this._premiumDelegate)
+      ? this._premiumDelegate.hasTalkback
+      : this._show.hasOwnProperty('talkback') && !this.isPeakDay;
   }
   get basePrice() {
     let result = this._show.price;
     if (this.isPeakDay) result += Math.round(result * 0.15);
-    return result;
+    return (this._premiumDelegate)
+      ? this._premiumDelegate.extendBasePrice(result)
+      : result;
+  }
+  get hasDinner() {
+    return (this._premiumDelegate)
+    ? this._premiumDelegate.hasDinner
+    : undefined;
+  }
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
   }
 }
-class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
     this._extras = extras;
   }
   get hasTalkback() {
-    return this._show.hasOwnProperty('talkback');
+    return this._host._show.hasOwnProperty('talkback');
   }
-  get basePrice() {
-    return Math.round(super.basePrice + this._extras.premiumFee);
+  extendBasePrice(base) {
+    return Math.round(base + this._extras.premiumFee);
   }
   get hasDinner() {
     return this._extras.hasOwnProperty('dinner') && !this.isPeakDay;
   }
 }
-aBooking = new Booking(show,date);
-aBooking = new PremiumBooking(show, date, extras);
+function createBooking(show, date) {
+  return new Booking(show, date);
+}
+function createPremiumBooking(show, date, extras) {
+  const result = new Booking(show, date);
+  result._bePremium(extras);
+  return result;
+}
+aBooking = createBooking(show,date);
+aBooking = createPremiumBooking(show, date, extras);
